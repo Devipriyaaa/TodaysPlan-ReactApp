c235c98c0608b7335bc094dac0dfe471fdb766a2
Devipriyaaaa <devipriyaprakashan7@gmail.com>
2024-01-01 21:58:34 +0530

    created add-todo page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Addtodo from './pages/Addtodo';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
-      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/addtodo' element={<Addtodo/>}  />
+
+
+        </Routes>
+
+
+
+
+      </BrowserRouter>
+
+      <Navbar />
     </div>
   );
 }
---
+++ src/pages/Addtodo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Addtodo = () => {
+  return (
+    <div className="text-white text-x">Addtodo</div>
+  )
+}
+
+export default Addtodo
