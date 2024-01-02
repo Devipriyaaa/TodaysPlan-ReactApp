9d6c4b768b5b2fa9d1fba52fcf7c7b8e0ec73893
Devipriyaaaa <devipriyaprakashan7@gmail.com>
2024-01-02 13:58:17 +0530

    footer created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Addtodo from './pages/Addtodo';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mainpage from './pages/Mainpage';
 
 function App() {
   return (
-    <div className="h-screen bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs7NB_wDPuL_HCf5PMoivtqik9azuf6TnJnw&usqp=CAU')">
+    <div className="h-screen bg-black">
 
 
 
       <BrowserRouter>
-      {/* <Navbar /> */}
+        {/* <Navbar /> */}
         <Routes>
           <Route path='/' element={<Mainpage />} />
           <Route path='/addtodo' element={<Addtodo />} />
@@ -21,7 +22,7 @@ function App() {
 
         </Routes>
 
-
+        <Footer />
 
 
       </BrowserRouter>
