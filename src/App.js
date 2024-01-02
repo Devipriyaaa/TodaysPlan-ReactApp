1632f5ae96239ae0b437b5c2732f39fc4375de63
jyothika <sarajyothika1234!@gmail.com>
2024-01-02 13:45:37 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar'
 import Addtodo from './pages/Addtodo';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mainpage from './pages/Mainpage';
+import Login from './pages/Login';
+import Forms from './pages/SigIn';
+import SigIn from './pages/SigIn';
 
 function App() {
   return (
@@ -17,6 +20,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Mainpage />} />
           <Route path='/addtodo' element={<Addtodo />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/SigIn'element={<SigIn/>}/>
 
 
 
---
+++ src/pages/SigIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Forms from '../components/Forms'
+
+const SigIn = () => {
+  return (
+    <div>
+      <Forms/>
+    </div>
+  )
+}
+
+export default SigIn
