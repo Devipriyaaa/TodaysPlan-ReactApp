5e17c8528e72a8e73e6c4613bd6ae850646c16e6
saniyafrancis <saniyafrancis7@gmail.com>
2024-02-20 19:09:56 +0530

    Addtodo updated today

--- src/pages/Addtodo.jsx
+++ src/pages/Addtodo.jsx
@@ -25,56 +25,92 @@ const Addtodo = () => {
   };
   const [pendingTodos,changePendingTodos] =useState(['Learn python']);
   const [completedTodos,changeCompletedTodos] =useState(['Learn typescript']);
-  const [todotext,changeTodotext] = useState('');
+  const [todoText,changeTodotext] = useState('');
+  const [errorVisible,changeErrorVisible] =useState(false);
+  const [error,changeError] =useState('')
 
   // funcvtion to update todo text
   const handleInputChange =(event)=> {
+    if(event.target.value.length>2){
+      changeError('')
+    }
   
   changeTodotext(event.target.value);
   }
- 
-  const addTodo =() =>(
-    changePendingTodos([...pendingTodos,todotext])
-  )
+ // to add todo in pending todos
+  const addTodo =()=>{
+    if(todoText.length>2){
+      changePendingTodos([...pendingTodos,todoText]);
+    changeTodotext('')
+    }
+    else{
+      changeError('Todo text should be minimum 3 characters')
+    }
+  }
+  // handle cancel button
+  const handleCancel =()=>{
+    changeTodotext('')
+  }
+  // clear todo sections
+  const clearTodos =(section)=>{
+    if(section==='pending'){
+      changePendingTodos([]);
+    }
+    else{
+      changeCompletedTodos([]);
+    }
+  }
+  const completeTodo = (index)=>{
+    const element =pendingTodos[index];
+    changeCompletedTodos([...completedTodos,element]);
+    const currentPendingItems =[...pendingTodos];
+    currentPendingItems.splice(index,1);
+    changePendingTodos(currentPendingItems);
+  }
+  const deleteTodo = (index,from) =>{
+    if(from==='pending'){
+      const currentPendingItems = [...pendingTodos];
+      currentPendingItems.splice(index,1);
+      changePendingTodos(currentPendingItems);
+    }
+    else{
+      const currentCompletedItems = [...completedTodos];
+      currentCompletedItems.splice(index,1);
+      changeCompletedTodos(currentCompletedItems);
+    }
+
+  }
+
   return (
     <>
-    <div className="text-black p-40">
-      <form onSubmit={handleSubmit(myfunc)} class="max-w-sm mx-auto">
-        <div class="mb-5">
-          <label
-            for="text"
-            class="block mb-2 text-3xl font-medium text-gray-900 text-black"
-          >
-            Today's Plan
-          </label>
-          <input type="text-black" value={todotext} onChange={handleInputChange} /> 
-        </div>
-        <div className="todo-form-buttons">
-        <Button class="add-btn" handleClick={()=>{}} btnText="Add" >Add</Button>
-        <Button class="cancel-btn" handleClick={()=>{}} btnText="Cancel" >Cancel</Button>
-        </div>
-      </form>
-      <p>{Add.task}</p>
+    <div className="todo-form text-2xl">
+      <h1>Add Todo</h1>
+      <input type="text" placeholder="Type todo" value={todoText} onChange={handleInputChange} />
+      <span className="error">{error}</span>
+      <div className="todo-form-buttons">
+        <button class="add-btn" handleClick={()=>addTodo()}>Add</button> 
+        <button class="cancel-btn" handleClick={()=>handleCancel()}>Cancel</button>
+      </div>
     </div>
     <div className="todo-section">
       <div className="todo-left">
-        <h1>Pending Todos ({pendingTodos.length}) <button class="add-btn" handleClick={()=>{}}>Clear</button> </h1>
+        <h1>Pending Todos ({pendingTodos.length}) <button class="add-btn" handleClick={()=>clearTodos('pending')}>Clear</button> </h1>
         {
           pendingTodos.map((todo,index)=><div className="todo.item">
-            <div className="todo.item.text">({index+1}){todo}</div>
-            <div className="todo-form-buttons"> <button class="add-btn" handleClick={()=>addTodo()}>Complete</button>
-            <button class="add-btn" handleClick={()=>{}}>Delete</button>
+            <div className="todo.item.text"> ({index+1}) {todo}</div>
+            <div className="todo-form-buttons"> <button class="add-btn" handleClick={()=>completeTodo(index)}>Complete</button>
+            <button class="delete-btn" handleClick={()=>deleteTodo(index,'pending')}>Delete</button>
             </div>
           </div>)
         }
       </div>
       <div className="todo-right">
-      <h1>Completed Todos ({completedTodos.length}) <button class="add-btn" handleClick={()=>{}}>Clear</button></h1>
+      <h1>Completed Todos ({completedTodos.length}) <button class="add-btn" handleClick={()=>clearTodos('completed')}>Clear</button></h1>
       {
          completedTodos.map((todo,index)=><div className="todo.item">
          <div className="todo.item.text">({index+1}){todo}</div>
          <div className="todo-form-buttons"> 
-         <button class="add-btn" handleClick={()=>{}}>Delete</button>
+         <button class="delete-btn" handleClick={()=>deleteTodo(index,'completed')}>Delete</button>
          </div>
        </div>)
       }
