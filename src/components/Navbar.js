27d43a30d4ac1e2ac4b5bf8cc5621c56ad5f7f4d
Devipriyaaaa <devipriyaprakashan7@gmail.com>
2024-02-21 11:06:46 +0530

    navbar up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => {
 <img className='w-60' src="https://www.filepicker.io/api/file/jsrJwNHkQRFuXd8MUiF7"/>
 
       <div className=" text-white font-bold hidden md:flex flex-col md:flex-row gap-5 items-center">
-        <p className="border-4 border-white rounded-md p-1">Home</p>
+        <p className="border-2 border-white rounded-md p-1">Home</p>
       
-       <select className="text-black font-bold border-4 border-white bg-red:500 rounded-md p-1">
+       <select className="text-black font-bold border-2 border-white bg-red:500 rounded-md p-1">
 
           <option value="English">English</option>
           <option value="Hindi">Hindi</option>
@@ -22,9 +22,6 @@ const Navbar = () => {
         </select>
 
         <button className='text-white font-bold border-2 border-white rounded-md  p-1'>
-=======
-        
-
             SignIn
           </button>
 
